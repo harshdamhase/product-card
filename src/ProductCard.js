efaf527b20dd452bf0e4a46aff54632799896e3d
Harshda Mhase <harshadamhase12@gmail.com>
2023-10-09 05:02:34 -0400

    product card added

--- src/ProductCard.js
+++ src/ProductCard.js
@@ -2,42 +2,47 @@ import React from 'react'
 import './ProductCard.css'
 import { useState } from 'react'
 
-function ProductCard({img, title, btnInc,btnDec}) {
-
+function ProductCard({ img, title, btnInc, btnDec }) {
     const [count, setCount] = useState(0);
 
-    function increment() {
-        count = count + 1;
-        console.log(count)
+    function Increment() {
+        setCount(count + 1);
+        console.log(count);
     }
 
     function decrement() {
-        count = count - 1;
+        setCount(count - 1);
         console.log(count);
     }
 
     function buyNow() {
         alert("Card added sucessfully!!");
     }
+
     return (
 
-            <div className='container'>
+        <div className='container'>
 
-             <div>
-             <img src={img} alt="img" className='img'/>
-             </div>   
+            <div>
+                <img src={img} alt="img" className='img' />
+            </div>
 
-                <p className='title'>{title}</p>
+            <p className='img-title'>{title}</p>
 
-                <button onClick={increment} className='btn'>{btnInc}</button>
+            <div className='btn-container'>
+                <button className='btn' onClick={Increment}>{btnInc}</button>
 
                 <h1 className="count">{count}</h1>
 
                 <button onClick={decrement} className='btn'>{btnDec}</button>
-
-                <p onClick={buyNow} className='btn-buy-now'>Buy Now</p>
-
             </div>
+
+            <div>
+                <button onClick={buyNow} className='btn buy-now'>Buy Now</button>
+            </div>
+
+
+        </div>
 
     )
 }
--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import ProductCard from './ProductCard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div className='content'>
+  <div>
   <h1 className="title">Product Card</h1>
   <div className="main-container">
-  <ProductCard img={""} title='lakme' btnInc={<i class="fa-solid fa-plus"></i>} btnDec={<i class="fa-solid fa-minus"></i>} />
+  <ProductCard img={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsg6roZK6RCQXY-WKOMZb6CJIij0sCi2rl-Q&usqp=CAU"} title='jam & catechup' btnInc={'+'} btnDec={'-'} />
+  <ProductCard img={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9gfoVg3Odf8JWSqE8LTPONBpPlD0sqGLG-Q&usqp=CAU"} title='Rice' btnInc={'+'} btnDec={'-'}/>
+  <ProductCard img={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSevzeX0B1a8o7bBEJp5Cz-E9dpTo8Ts-1QbQ&usqp=CAU"} title="vegetables" btnInc={'+'} btnDec={'-'}/>
+  
+  {/* <ProductCard/>
+  <ProductCard/> */}
   </div>
   </div>
 );
